a273abb7376facd39ef2443294cfe383221e4607
Abhishek Pandya <abhishekp213@hotmail.com>
2020-12-12 01:49:51 -0500

    spacing issues fixed for social media icons

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import PropTypes from "prop-types"
 import React from "react"
 import "./header.css"
 import { AiOutlineTwitter, AiOutlineGithub, AiFillLinkedin, AiOutlineFilePdf, AiOutlineMail } from 'react-icons/ai';
-import {Box} from "rebass"
+import {Box, Flex, Text, } from "rebass"
 // gradient from https://www.gradient-animator.com/
 
 const Header = ({ siteTitle }) => (
@@ -23,29 +23,15 @@ const Header = ({ siteTitle }) => (
           <a href='/' style = {{color: 'white', textDecoration: 'none', textShadow: 'none', backgroundImage: 'none'}}>
             {siteTitle} 
           </a>
-        </h1>
-      </div>
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `1.45rem 1.0875rem`,
-          alignItems: 'right',
-        }}
-      >
-        <h1
-          style={{ 
-            margin: 0,
-            color: 'white',
-        }}>
-          <a href='mailto:abpandya@seas.upenn.edu' style = {{color: 'white', textDecoration: 'none', textShadow: 'none', backgroundImage: 'none'}}> <AiOutlineMail /> </a>
+          <a href='mailto:abpandya@seas.upenn.edu' style = {{color: 'white', paddingLeft: '34.5%', textDecoration: 'none', textShadow: 'none', backgroundImage: 'none'}}> <AiOutlineMail /> </a>
           <a href='https://github.com/abhishekp106' style = {{color: 'white', textDecoration: 'none', textShadow: 'none', backgroundImage: 'none'}}> <AiOutlineGithub /> </a>
           <a href='https://twitter.com/abhishekp106' style = {{color: 'white', textDecoration: 'none', textShadow: 'none', backgroundImage: 'none'}}> <AiOutlineTwitter /> </a>
           <a href='https://www.linkedin.com/in/abhishekanujpandya/' style = {{color: 'white', textDecoration: 'none', textShadow: 'none', backgroundImage: 'none'}}> <AiFillLinkedin /> </a>
           <a href='https://abhishekp106.github.io/resume_latex.pdf' style = {{color: 'white', textDecoration: 'none', textShadow: 'none', backgroundImage: 'none'}}> <AiOutlineFilePdf /> </a>
         </h1>
+        
       </div>
-    
+      
   </header>
 )
 
